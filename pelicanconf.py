f18ce970f6a2834d50762e1af5e5ee8b1c9ab709
wantong <liwantong@163.com>
2025-03-11 03:50:01 +0000

    update pelican cofnig file with new theme and layout

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,18 +1,38 @@
 AUTHOR = 'winter'
-SITENAME = "winter's knowledge share"
-SITEURL = ""
+SITENAME = "Sumarization"
+SITEURL = "https://wantong.github.io"
+
+SITETITLE = AUTHOR
+SITESUBTITLE = u"concentrate"
+SITEDESCRIPTION = "Tight your thought, fast your work flow"
+SITELOGO = SITEURL + '/images/wantongtop.png'
+FAVICON = SITEURL + '/images/web_icon_64x64.icon'
+BROWSER_COLOR = '#333333'
+RELATIVE_URLS = True
+
+SOCIAL = (('envelope', 'mailto:guqian110@163.com'),
+    ('github','https://github.com/wantong'),
+        )
+
+MENUITEMS = (('Authors', '/authors.html'),
+             ('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),
+             )
 
 PATH = "content"
 
 TIMEZONE = 'Asia/Shanghai'
 
-DEFAULT_LANG = 'zh_CN'          # 默认语言设为中文
-OG_LOCALE = 'zh_CN'          # Open Graph 元数据语言
-LOCALE = 'zh_CN.UTF-8'       # 本地化设置（确保系统已安装此语言包）
+DEFAULT_LANG = 'zh_CN'          # 默�?��??言设为�?�?
+OG_LOCALE = 'zh_CN'          # Open Graph 元数�?�?言
+LOCALE = 'zh_CN.UTF-8'       # �?地化设置（确保系统已安�?��?��??言包）
 
-BIND = "0.0.0.0"  # 允许外部访问
-PORT = 3588          # 指定端口
+BIND = "0.0.0.0"  # 允�?��?�部访问
+PORT = 3588          # 指定�?�?
 
+THEME = 'Flex'
+THEME_COLOR = 'light'
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -20,22 +40,29 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 # pelicanconf.py
+
+
+ROBOTS = "index, follow"
+
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa"
+}
+
+COPYRIGHT_YEAR = 2025
+
+STATIC_PATHS = ["extra/custom.css"]
+
+# Enable i18n plugin.
+# PLUGINS = ["i18n_subsites"]
+# Enable Jinja2 i18n extension used to parse translations.
+# JINJA_ENVIRONMENT = {"extensions": ["jinja2.ext.i18n"]}
+
+MAIN_MENU = True
